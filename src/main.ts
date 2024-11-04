f44c31ce4f73c481d0c35b9e8566329bb4da4cfc
Jobin <425605679@qq.com>
2024-11-04 15:57:57 +0800

    chore: upgrade deps

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import router from './router'
 import './assets/main.css'
+
 import 'virtual:uno.css'
 import 'the-new-css-reset/css/reset.css'
-
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
-import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
 
